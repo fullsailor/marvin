c7e6b2a739bf031f5de34e6f791495aa6d0644bd
Andrew  Smith <andrew@envylabs.com>
2011-03-31 00:00:31 -0400

    He lives!

---
+++ marvin.rb
@@ -0,0 +1,97 @@
+require 'bundler'
+Bundler.setup
+
+require 'tinder'
+require 'yajl'
+require 'yajl/http_stream'
+require 'yaml'
+
+module Marvin
+
+
+  def self.configure
+    config = YAML.load_file('./config.yml')
+    @@token = config['token']
+    @@subdomain = config['subdomain']
+  end
+
+  def self.token
+    @@token
+  end
+
+  def self.subdomain
+    @@subdomain
+  end
+
+  def self.lives!
+    mb = Marvin::Bot.new
+    sleep(2)
+    mb.run
+  end
+
+  class Bot
+
+    def initialize
+      @campfire = Tinder::Campfire.new( Marvin.subdomain, :token => Marvin.token )
+      join_room!
+    end
+
+    def room_uri
+      @room_uri ||= URI.parse(
+        "http://#{Marvin.token}:x@streaming.campfirenow.com//room/#{@room.id}/live.json"
+      )
+    end
+
+    def run
+      Yajl::HttpStream.get(room_uri, :symbolize_keys => true) do |message|
+        case message[:type]
+        when 'TextMessage'
+          handle_message(message)
+        else
+          puts "not processing #{message[:type]}"
+        end
+      end
+    ensure
+      leave_room!
+    end
+
+    def join_room!
+      @room = @campfire.rooms.first
+      @room.join
+      say "Mornin!"
+      self
+    end
+
+    def leave_room! 
+      say "Goodbye forever"
+      @room.leave
+      self
+    end
+
+    def say(text)
+      @room.speak(text)
+      self
+    end
+
+
+    private
+
+    def handle_message(message)
+      case message[:body]
+      when /^marvin\ /i
+        say "Are you talking to me?"
+      when /marvin/i
+        say "Are you talking about me?"
+      when /^y/i
+        say "Fantastic!"
+      when /^n/i
+        say "Well why not?"
+      end
+    end
+
+  end
+
+end
+
+Marvin.configure
+Marvin.lives!
